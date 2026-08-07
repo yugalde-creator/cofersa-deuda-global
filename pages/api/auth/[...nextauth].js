178e9c7bbe2a7da854ea289feb297b0ea0207c7a
yugalde-creator <yugalde@cofersa.cr>
2026-08-07 13:07:17 -0600

    Add pages/api/auth/[...nextauth].js

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,24 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      session.user.email = token.email;
+      return session;
+    },
+    async jwt({ token, account }) {
+      return token;
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
+});
